73739128a244b1f0082385499af62d3d61510a50
koushikdandi <147510409+Koushikdandi@users.noreply.github.com>
2026-03-20 18:13:46 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (40.97%) - LeetSync

---
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) 
+    {
+        int a=0,b=0;
+        vector<int>ans;
+        for(int i=0;i<m+n;i++)
+        {
+            if(a<m && b<n && nums1[a]<=nums2[b])
+            {
+                ans.push_back(nums1[a]);
+                a++;
+            }
+            else if(a>=m)
+            {
+                ans.push_back(nums2[b]);
+                b++;
+            }
+            
+            else if(a<m && b<n && nums1[a]>=nums2[b])
+                {
+                    ans.push_back(nums2[b]);
+                    b++;
+                }
+            
+            
+            else if(b>=n)
+            {
+                ans.push_back(nums1[a]);
+                a++;
+            }
+        }
+        for(int i=0;i<n+m;i++)
+        {
+            nums1[i]=ans[i];
+        }
+    }
+};
